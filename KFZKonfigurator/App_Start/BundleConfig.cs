948276f09e43fd7c2f79c14fbbfb57e695f36377
Lemao81 <jr.berlin@web.de>
2018-06-27 07:34:31 +0200

    Erstellung Konfigurator Controller + View; Erstellung + Einbindung Script + Styles Bundles

---
+++ KFZKonfigurator/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace KFZKonfigurator
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/scripts")
+                .Include("~/Scripts/jquery-3.3.1.js", "~/Scripts/bootstrap.js", "~/Scripts/bootstrap.bundle.js", "~/Scripts/knockout-3.4.2.js", "~/Scripts/underscore.js",
+                "~/Scripts/modernizr-2.6.2.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/styles")
+                .Include("~/Content/bootstrap.css", "~/Content/Site.css"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
---
+++ KFZKonfigurator/Controllers/KonfiguratorController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KFZKonfigurator.Controllers
+{
+    public class KonfiguratorController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
