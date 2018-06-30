516709af40cd44d310283ae620e7dee3064c19ef
Lemao81 <jr.berlin@web.de>
2018-06-30 07:53:46 +0200

    Neues Base Projekt zugefügt

---
+++ KFZKonfigurator.Base/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace KFZKonfigurator.Base
+{
+    public static class Extensions
+    {
+        public static string GetPropertyName<TModel, TProperty>(this Expression<Func<TModel, TProperty>> expression)
+        {
+            if (!(expression.Body is MemberExpression memberExpression) || !(memberExpression.Member is PropertyInfo propertyInfo))
+                throw new ArgumentException("Expression is not a property");
+
+            return propertyInfo.Name;
+        }
+    }
+}
