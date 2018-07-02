aec2aad755d4463f3b7bceac75e34753c7ca5340
Lemao81 <jr.berlin@web.de>
2018-07-02 19:46:02 +0200

    Data Projekt angelegt mit Kontextklasse + Dao

---
+++ KFZKonfigurator.Data/IDao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KFZKonfigurator.Data
+{
+    public interface IDao<TModel>
+    {
+        TModel Read(Guid id);
+
+        TModel Update(Guid id, string propertyName, object newValue);
+    }
+}
---
+++ KFZKonfigurator.Data/KonfiguratorContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+using KFZKonfigurator.BusinessModels.Model;
+
+namespace KFZKonfigurator.Data
+{
+    public class KonfiguratorContext : DbContext
+    {
+        public DbSet<CarConfiguration> Configurations { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
