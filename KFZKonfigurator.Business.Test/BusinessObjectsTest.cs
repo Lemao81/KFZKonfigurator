d392966572b110de7543bef84083ef48dea86024
Lemao81 <jr.berlin@web.de>
2018-07-02 19:42:40 +0200

    Projekt für Business Unit Tests angelegt + erste Tests

---
+++ KFZKonfigurator.Business.Test/BusinessObjectsTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using KFZKonfigurator.BusinessModels.Enum;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using KFZKonfigurator.Base.Attributes;
+
+namespace KFZKonfigurator.Business.Test
+{
+    [TestClass]
+    public class BusinessObjectsTest
+    {
+        [TestMethod]
+        public void Test_that_enums_have_price_attribute()
+        {
+            //Arrange
+            var actualEquipment = Enum.GetValues(typeof(Equipment)).OfType<Equipment>()
+                .All(_ =>
+                {
+                    var attributes = typeof(Equipment).GetMember(_.ToString())[0].GetCustomAttributes(typeof(PriceAttribute), false);
+                    return attributes.Any() && attributes[0] is PriceAttribute;
+                });
+
+            var actualRims = Enum.GetValues(typeof(Rims)).OfType<Rims>()
+                .All(_ =>
+                {
+                    var attributes = typeof(Rims).GetMember(_.ToString())[0].GetCustomAttributes(typeof(PriceAttribute), false);
+                    return attributes.Any() && attributes[0] is PriceAttribute;
+                });
+
+            //Assert
+            Assert.IsTrue(actualEquipment);
+            Assert.IsTrue(actualRims);
+        }
+
+        [TestMethod]
+        public void Test_that_price_strings_are_valid()
+        {
+            //Arrange
+            var actualEquipment = Enum.GetValues(typeof(Equipment)).OfType<Equipment>()
+                .All(_ =>
+                {
+                    var attribute = typeof(Equipment).GetMember(_.ToString())[0].GetCustomAttributes(typeof(PriceAttribute), false)[0] as PriceAttribute;
+                    return decimal.TryParse(attribute.Price, out var price);
+                });
+
+            var actualRims = Enum.GetValues(typeof(Rims)).OfType<Rims>()
+                .All(_ =>
+                {
+                    var attribute = typeof(Rims).GetMember(_.ToString())[0].GetCustomAttributes(typeof(PriceAttribute), false)[0] as PriceAttribute;
+                    return decimal.TryParse(attribute.Price, out var price);
+                });
+
+            //Assert
+            Assert.IsTrue(actualEquipment);
+            Assert.IsTrue(actualRims);
+        }
+    }
+}
---
+++ KFZKonfigurator.Business.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KFZKonfigurator.Business.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KFZKonfigurator.Business.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b14edbaa-f79d-424b-ba07-4f3af4ba642f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
