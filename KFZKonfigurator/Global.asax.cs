49160abe7df47ad0eeda946fd3c6005c6a6b2982
Lemao81 <jr.berlin@web.de>
2018-07-03 10:39:00 +0200

    Anpassung Dependency Injection

--- KFZKonfigurator/Global.asax.cs
+++ KFZKonfigurator/Global.asax.cs
@@ -5,6 +5,13 @@ using System.Web.Mvc;
 using System.Web.Routing;
 using System.Web.Http;
 using System.Web.Optimization;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using KFZKonfigurator.BusinessModels.Model;
+using KFZKonfigurator.BusinessModels.Services;
+using KFZKonfigurator.Controllers;
+using KFZKonfigurator.Data;
+using KFZKonfigurator.Services;
 using log4net.Config;
 
 namespace KFZKonfigurator
@@ -19,7 +26,19 @@ namespace KFZKonfigurator
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             XmlConfigurator.Configure(new FileInfo(Server.MapPath("~/Web.config")));
-            DiRegistrations.Setup();
+
+            SetupWindsorRegistrations();
+        }
+
+        public static void SetupWindsorRegistrations()
+        {
+            var container = new WindsorContainer();
+
+            ControllerBuilder.Current.SetControllerFactory(new DiControllerFactory(container));
+
+            container.Register(Component.For<IDao<CarConfiguration>>().ImplementedBy<CarConfigurationDao>());
+            container.Register(Component.For<CarConfigurationController>().ImplementedBy<CarConfigurationController>().LifeStyle.Transient);
+            container.Register(Component.For<PriceCalculationService>().ImplementedBy<PriceCalculationService>());
         }
     }
 }
