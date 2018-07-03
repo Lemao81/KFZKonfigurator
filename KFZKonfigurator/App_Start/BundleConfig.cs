3cc7f66a914b9f3f6c272be52f98a75f68154aa7
Lemao81 <jr.berlin@web.de>
2018-07-03 10:35:11 +0200

    Anpassung Scriptbundles

--- KFZKonfigurator/App_Start/BundleConfig.cs
+++ KFZKonfigurator/App_Start/BundleConfig.cs
@@ -6,15 +6,13 @@ namespace KFZKonfigurator
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/scripts")
-                .Include("~/Scripts/jquery-3.3.1.js", "~/Scripts/bootstrap.js", "~/Scripts/bootstrap.bundle.js", "~/Scripts/knockout-3.4.2.js",
-                    "~/Scripts/underscore.js",
-                    "~/Scripts/modernizr-2.6.2.js"));
+            bundles.Add(new ScriptBundle("~/bundles/knockout").Include("~/Scripts/knockout-3.4.2.js", "~/Scripts/knockout.mapping-latest.debug.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js", "~/Scripts/bootstrap.bundle.js"));
+            bundles.Add(new ScriptBundle("~/bundles/divers").Include("~/Scripts/jquery-3.3.1.js", "~/Scripts/underscore.js", "~/Scripts/modernizr-2.6.2.js", "~/Scripts/Custom/binding.js"));
 
-            bundles.Add(new StyleBundle("~/bundles/styles")
-                .Include("~/Content/bootstrap.css", "~/Content/Site.css"));
+            bundles.Add(new StyleBundle("~/bundles/styles").Include("~/Content/bootstrap.css", "~/Content/Site.css"));
 
-            BundleTable.EnableOptimizations = true;
+//            BundleTable.EnableOptimizations = true;
         }
     }
 }
--- KFZKonfigurator/App_Start/RouteConfig.cs
+++ KFZKonfigurator/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace KFZKonfigurator
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Konfigurator", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "CarConfiguration", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
